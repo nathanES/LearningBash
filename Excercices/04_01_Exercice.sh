0aa1cfd64e46aa1015a4f98fb31906977cb044f5
Nathan <nathan.esslinger67@gmail.com>
2025-04-06 14:02:23 +0200

    deleting old exercices

--- Excercices/04_01_Exercice.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-filename=$1
-filename=${filename:?"missing"}
-howmany=${2:-10}
-sort -nr $filename | head -$howmany
--- Excercices/04_02_Exercice.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-outfile=${filename%.pcx}.jpg # looks for pcx on the end of the string. If it's found it's stripped off and the rest of the string is return. If no match, nothing is deleted

ca7adbc0b597a574dc23d1b496ee96db8f95f210
Nathan <nathan.esslinger67@gmail.com>
2025-04-07 13:11:18 +0200

    update exercice

--- Exercices/04_02_Exercice.sh
+++ Exercices/04_02_Exercice.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 outfile=${filename%.pcx}.jpg # looks for pcx on the end of the string. If it's found it's stripped off and the rest of the string is return. If no match, nothing is deleted
+for filename do
+  pnmfile=${filename%.*}.ppm
+
+  case $filename in
+    *.jpg) exit 0;;
+    *.tga) tgatoppm $filename > $pnmfile;;
+    *.xpm) xpmtoppm $filename > $pnmfile;;
+    *.pcx) pcxtoppm $filename > $pnmfile;;
+    *.tif) tifftopnm $filename > $pnmfile;;
+    *.gif) giftoppm $filename > $pnmfile;;
+    *) echo "procfile: $filename is an unknown graphics file."
+      exit 1;;
+outfile=${pnmfile%.ppm}.new.jpg
+pnmtojpeg $pnmfile > $outfile
+
+rm $pnmfile
+done
